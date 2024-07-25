5ad6712ef21c2f23ad0dc7fc44cc795b4642a767
Saksham <saksham.gairola28@gmail.com>
2024-07-25 21:08:55 +0530

    initial commit

---
+++ src/main/java/com/service/notifications_core/NotificationsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.service.notifications_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/notifications_core/NotificationsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.notifications_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
